267c78a38a6b92c87323a7df029cc5e15df10fe1
Chris Simmons <chris@turing.edu>
2021-11-11 12:08:19 -0500

    iteration 2

---
+++ lib/potluck.rb
@@ -0,0 +1,17 @@
+class Potluck
+  attr_reader :date,
+              :dishes
+
+
+  def initialize(date, dishes = [])
+    @date = date
+    @dishes = dishes
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+
+
+end
--- test/dish_spec.rb
+++ test/dish_spec.rb
@@ -1,13 +1,47 @@
 require './lib/dish'
+require './lib/potluck'
 
 dish = Dish.new("Couscous salad", :appetizer)
+dish_2 = Dish.new("Meatballs", :entree)
+potluck = Potluck.new("7-13-18")
+potluck.add_dish(dish)
+potluck.add_dish(dish_2)
+
 
 RSpec.describe Dish do
   it 'exists' do
     expect(dish).to be_instance_of(Dish)
   end
 
+  it 'has name' do
+    expect(dish.name).to eq("Couscous salad")
+  end
+  it 'has category' do
+    expect(dish.category).to eq(:appetizer)
+  end
+
+
+
 end
+
+RSpec.describe Potluck do
+  it 'exists' do
+    expect(potluck).to be_instance_of(Potluck)
+  end
+
+  it 'has date' do
+    expect(potluck.date).to eq("7-13-18")
+  end
+
+  it 'has dishes' do
+    expect(potluck.dishes).to be_instance_of(Array)
+  end
+
+  it 'has 2 dishes' do
+    expect(potluck.dishes.length).to eq(2)
+  end
+end
+
 
 
 # require 'pry'; binding.pry;
